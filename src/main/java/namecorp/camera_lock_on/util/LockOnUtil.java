2647a20ebad1f7f012dbeac6123e4d01ab40acc5
namecorp-llc <namecorpllc@gmail.com>
2025-05-22 13:54:33 -0700

    Update LockOnUtil.java
    Moved raycast from LockOnEvent to Util

--- src/main/java/namecorp/camera_lock_on/util/LockOnUtil.java
+++ src/main/java/namecorp/camera_lock_on/util/LockOnUtil.java
@@ -21,4 +21,34 @@ public class LockOnUtil {
         }
     }
 
+    public static HitResult ensureTargetInRange(HitResult hitResult, Vec3d cameraPos, double interactionRange) {
+        Vec3d vec3d = hitResult.getPos();
+        if (!vec3d.isInRange(cameraPos, interactionRange)) {
+            Vec3d vec3d2 = hitResult.getPos();
+            Direction direction = Direction.getFacing(vec3d2.x - cameraPos.x, vec3d2.y - cameraPos.y, vec3d2.z - cameraPos.z);
+            return BlockHitResult.createMissed(vec3d2, direction, BlockPos.ofFloored(vec3d2));
+        } else {
+            return hitResult;
+        }
+    }
+    public static HitResult raycastCrosshair(Entity camera, double range, float tickDelta) {
+        double d = range;
+        double e = MathHelper.square(range);
+        Vec3d vec3d = camera.getCameraPosVec(tickDelta);
+        HitResult hitResult = camera.raycast(d, tickDelta, false);
+        double f = hitResult.getPos().squaredDistanceTo(vec3d);
+        if (hitResult.getType() != HitResult.Type.MISS) {
+            e = f;
+            d = Math.sqrt(f);
+        }
+
+        Vec3d vec3d2 = camera.getRotationVec(tickDelta);
+        Vec3d vec3d3 = vec3d.add(vec3d2.x * d, vec3d2.y * d, vec3d2.z * d);
+
+        Box box = camera.getBoundingBox().stretch(vec3d2.multiply(d)).expand(4.0, 4.0, 4.0);
+        EntityHitResult entityHitResult = ProjectileUtil.raycast(camera, vec3d, vec3d3, box, entity -> !entity.isSpectator() && entity.canHit(), e);
+        return entityHitResult != null && entityHitResult.getPos().squaredDistanceTo(vec3d) < f
+                ? ensureTargetInRange(entityHitResult, vec3d, range)
+                : ensureTargetInRange(hitResult, vec3d, range);
+    }
 }
