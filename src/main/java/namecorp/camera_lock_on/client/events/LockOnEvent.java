811106a5ca2d083fbe62696bc55b1217f9ff8f8c
namecorp-llc <namecorpllc@gmail.com>
2025-05-22 13:54:08 -0700

    Update LockOnEvent.java
    Fixed jitter at large FPS numbers and generally cleaned code. Help from Exopandora

--- src/main/java/namecorp/camera_lock_on/client/events/LockOnEvent.java
+++ src/main/java/namecorp/camera_lock_on/client/events/LockOnEvent.java
@@ -14,177 +14,122 @@ import net.minecraft.util.math.*;
 
 import static namecorp.camera_lock_on.client.Camera_lock_onClient.*;
 
-public class LockOnEvent implements WorldRenderEvents.Last {
+public class LockOnEvent implements WorldRenderEvents.Start {
     @Override
-    public void onLast(WorldRenderContext last) {
+    public void onStart(WorldRenderContext worldRenderContext) {
         MinecraftClient client = MinecraftClient.getInstance();
         ClientPlayerEntity player = client.player;
 
+        if(player == null || client.world == null) { lockedEntity = null; return; }
+
         float tickDelta = worldRenderContext.tickCounter().getTickDelta(false);
-        
-        if(player != null && client.world != null) {
-            if(lockOnKeyBind.wasPressed()) {
-                HitResult hit = raycastCrosshair(client.cameraEntity, 500.0f, tickDelta);
-                if(lockedEntity != null) {
-                    lockedEntity = null;
-                } else {
-                    if(hit instanceof EntityHitResult && ((EntityHitResult) hit).getEntity() instanceof LivingEntity) {
-                        lockedEntity = (LivingEntity) ((EntityHitResult) hit).getEntity();
-                    } else {
-                        float currYaw = player.getYaw(tickDelta);
-                        float tempYaw = currYaw;
-                        float savedYaw = 999999;
 
-                        LivingEntity temp = null;
-
-                        for(int i = -20; i < 20; i++) {
-                            tempYaw = currYaw + Math.abs(i);
-                            player.setYaw(currYaw+i);
-                            HitResult hit1 = raycastCrosshair(client.cameraEntity, 500.0f, tickDelta);
-
-                            if(hit1 instanceof EntityHitResult && ((EntityHitResult) hit1).getEntity() instanceof LivingEntity) {
-                                LivingEntity newEntity = (LivingEntity) ((EntityHitResult) hit1).getEntity();
-                                if(temp == null) {
-                                    temp = (LivingEntity) ((EntityHitResult) hit1).getEntity();
-                                } else {
-                                    if(tempYaw < savedYaw) {
-                                        temp = newEntity;
-                                        savedYaw = tempYaw;
-                                    }
-                                }
-
-                            }
-                        }
-
-                        if(temp != null) {
-                            lockedEntity = temp;
-                        }
-
-                        player.setYaw(currYaw);
-                    }
-                }
-            }
-
+        // Lock onto entity
+        if(lockOnKeyBind.wasPressed()) {
+            HitResult hit = raycastCrosshair(client.cameraEntity, 500.0f, tickDelta);
             if(lockedEntity != null) {
-                if(!lockedEntity.isAlive()) {
+                lockedEntity = null;
+            } else {
+                if(hit instanceof EntityHitResult && ((EntityHitResult) hit).getEntity() instanceof LivingEntity) {
+                    lockedEntity = (LivingEntity) ((EntityHitResult) hit).getEntity();
+                } else {
+                    float currYaw = player.getYaw(tickDelta);
+                    float tempYaw;
+                    float savedYaw = 999999;
+
+                    LivingEntity tempEntity = null;
+
+                    for(int i = -20; i < 20; i++) {
+                        tempYaw = currYaw + Math.abs(i);
+                        player.setYaw(currYaw+i);
+                        HitResult hit1 = raycastCrosshair(client.cameraEntity, 500.0f, tickDelta);
+
+                        if(hit1 instanceof EntityHitResult && ((EntityHitResult) hit1).getEntity() instanceof LivingEntity) {
+                            LivingEntity newEntity = (LivingEntity) ((EntityHitResult) hit1).getEntity();
+                            if(tempEntity == null) {
+                                tempEntity = (LivingEntity) ((EntityHitResult) hit1).getEntity();
+                            } else {
+                                if(tempYaw < savedYaw) {
+                                    tempEntity = newEntity;
+                                    savedYaw = tempYaw;
+                                }
+                            }
+
+                        }
+                    }
+
+                    if(tempEntity != null) {
+                        lockedEntity = tempEntity;
+                    }
+
+                    player.setYaw(currYaw);
+                }
+            }
+        }
+
+        // Stops code if no entity is currently locked on
+        if(lockedEntity == null) return;
+
+        // Lose locked entity if the player cannot see it for more than 3 seconds
+        if(!lockedEntity.isAlive()) {
+            lockedEntity = null;
+        } else {
+            if(!player.canSee(lockedEntity) && !player.hasPermissionLevel(2)) {
+                if(noSightTimer > 60) {
+                    noSightTimer = 0;
                     lockedEntity = null;
                 } else {
-                    if(!player.canSee(lockedEntity) && !player.hasPermissionLevel(2)) {
-                        if(noSightTimer > 60) {
-                            noSightTimer = 0;
-                            lockedEntity = null;
-                        } else {
-                            noSightTimer++;
-                        }
-                    } else {
-                        noSightTimer = 0;
-                    }
+                    noSightTimer++;
                 }
+            } else {
+                noSightTimer = 0;
             }
+        }
 
-            if (lockedEntity != null) {
-                Vec3d targetPos = lockedEntity.getPos();
-                Vec3d playerPos = player.getPos();
+        // Actual Math Here
+        Vec3d targetPos = lockedEntity.getPos();
+        Vec3d playerPos = player.getPos();
 
-                float lockedYaw;
-                float lockedPitch;
+        float lockedYaw;
+        float lockedPitch;
 
-                float currentYaw = player.getYaw(tickDelta);
-                float currentPitch = player.getPitch(tickDelta);
-
-                double deltaX = Math.abs(targetPos.x - playerPos.x);
-                double deltaY = targetPos.y - playerPos.y;
-                double deltaZ = Math.abs(targetPos.z - playerPos.z);
-
-                if(lockedEntity.getHeight() < 1.5f) {
-                    deltaY--;
-                } else if (lockedEntity.getHeight() < 2) {
-                    deltaY -= 0.3f;
-                }
-
-                double thetaRadiansXY = Math.atan2(Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaZ, 2)), deltaY);
-                double thetaDegreesXY = Math.toDegrees(thetaRadiansXY);
-
-                if(targetPos.z >= playerPos.z) {
-                    double thetaRadiansXZ = Math.atan2(deltaX, deltaZ);
-                    double thetaDegreesXZ = Math.toDegrees(thetaRadiansXZ);
-                    if(targetPos.x >= playerPos.x) {
-                        lockedYaw = (float) -thetaDegreesXZ;
-                    } else {
-                        lockedYaw = (float) thetaDegreesXZ;
-                    }
-                } else {
-                    double thetaRadiansXZ = Math.atan2(deltaZ, deltaX);
-                    double thetaDegreesXZ = Math.toDegrees(thetaRadiansXZ);
-                    if(targetPos.x >= playerPos.x) {
-                        lockedYaw = (float) -thetaDegreesXZ - 90;
-                    } else {
-                        lockedYaw = (float) thetaDegreesXZ + 90;
-                    }
-                }
+        double deltaX = Math.abs(targetPos.x - playerPos.x);
+        double deltaY = targetPos.y - playerPos.y;
+        double deltaZ = Math.abs(targetPos.z - playerPos.z);
 
 
-                lockedPitch = (float) thetaDegreesXY - 90;
+        if(lockedEntity.getHeight() < 1.5f) {
+            deltaY--;
+        } else if (lockedEntity.getHeight() < 2) {
+            deltaY -= 0.3f;
+        }
 
-                if (lockedYaw < currentYaw - 180f) {
-                    lockedYaw += 360f;
-                }
 
-                if (lockedYaw > currentYaw + 180f) {
-                    lockedYaw -= 360f;
-                }
+        double thetaRadiansXY = Math.atan2(Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaZ, 2)), deltaY);
+        double thetaDegreesXY = Math.toDegrees(thetaRadiansXY);
 
-                float yawDelta = cameraDelta;
-                float pitchDelta = cameraDelta;
-
-                if(player.input.jumping) {
-                    pitchDelta = cameraDelta/5f;
-                }
-
-                if(client.getCurrentFps() > 200) {
-                    int i = client.getCurrentFps()/200;
-                    for(int j = 0; j < i; j++) {
-                        yawDelta = yawDelta/2;
-                        pitchDelta = pitchDelta/2;
-                    }
-                }
-
-                player.setYaw(MathHelper.lerp(yawDelta, currentYaw, lockedYaw));
-                player.setPitch(MathHelper.lerp(pitchDelta, currentPitch, lockedPitch));
+        if(targetPos.z >= playerPos.z) {
+            double thetaRadiansXZ = Math.atan2(deltaX, deltaZ);
+            double thetaDegreesXZ = Math.toDegrees(thetaRadiansXZ);
+            if(targetPos.x >= playerPos.x) {
+                lockedYaw = (float) -thetaDegreesXZ;
+            } else {
+                lockedYaw = (float) thetaDegreesXZ;
             }
         } else {
-            lockedEntity = null;
-        }
-    }
-
-    public static HitResult ensureTargetInRange(HitResult hitResult, Vec3d cameraPos, double interactionRange) {
-        Vec3d vec3d = hitResult.getPos();
-        if (!vec3d.isInRange(cameraPos, interactionRange)) {
-            Vec3d vec3d2 = hitResult.getPos();
-            Direction direction = Direction.getFacing(vec3d2.x - cameraPos.x, vec3d2.y - cameraPos.y, vec3d2.z - cameraPos.z);
-            return BlockHitResult.createMissed(vec3d2, direction, BlockPos.ofFloored(vec3d2));
-        } else {
-            return hitResult;
-        }
-    }
-    public static HitResult raycastCrosshair(Entity camera, double range, float tickDelta) {
-        double d = range;
-        double e = MathHelper.square(range);
-        Vec3d vec3d = camera.getCameraPosVec(tickDelta);
-        HitResult hitResult = camera.raycast(d, tickDelta, false);
-        double f = hitResult.getPos().squaredDistanceTo(vec3d);
-        if (hitResult.getType() != HitResult.Type.MISS) {
-            e = f;
-            d = Math.sqrt(f);
+            double thetaRadiansXZ = Math.atan2(deltaZ, deltaX);
+            double thetaDegreesXZ = Math.toDegrees(thetaRadiansXZ);
+            if(targetPos.x >= playerPos.x) {
+                lockedYaw = (float) -thetaDegreesXZ - 90;
+            } else {
+                lockedYaw = (float) thetaDegreesXZ + 90;
+            }
         }
 
-        Vec3d vec3d2 = camera.getRotationVec(tickDelta);
-        Vec3d vec3d3 = vec3d.add(vec3d2.x * d, vec3d2.y * d, vec3d2.z * d);
-        float g = 1.0F;
-        Box box = camera.getBoundingBox().stretch(vec3d2.multiply(d)).expand(4.0, 4.0, 4.0);
-        EntityHitResult entityHitResult = ProjectileUtil.raycast(camera, vec3d, vec3d3, box, entity -> !entity.isSpectator() && entity.canHit(), e);
-        return entityHitResult != null && entityHitResult.getPos().squaredDistanceTo(vec3d) < f
-                ? ensureTargetInRange(entityHitResult, vec3d, range)
-                : ensureTargetInRange(hitResult, vec3d, range);
+
+        lockedPitch = (float) thetaDegreesXY - 90;
+
+        player.setYaw(MathHelper.lerpAngleDegrees(tickDelta*cameraDelta, player.prevYaw, lockedYaw));
+        player.setPitch(MathHelper.lerpAngleDegrees(tickDelta*cameraDelta, player.prevPitch, lockedPitch));
     }
 }
